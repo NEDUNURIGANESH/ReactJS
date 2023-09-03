a4473fbfc1c066fec59c4cda19d5cd3fca67e523
Ganesh Nedunuri <ganeshnedunuri3339@gmail.com>
2023-09-03 14:08:28 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name="Ganesh";
+const date=new Date();
+const current_year=date.getFullYear();
+
 ReactDOM.render(
   <div>
     <div>
@@ -13,8 +17,8 @@ ReactDOM.render(
     </div>
     <div>
       <h1>creating my name with copyright symbol</h1>
-    <p>Ganesh</p>
-    <p>&copy;2023</p>
+    <p>Created by {name}</p>
+    <p>Copyright &copy; {current_year}</p>
     </div>
   </div>,
   document.getElementById("root")
