5c7a4c08fb8e65f921df03d4c3f1d7f51126ab8b
NEDUNURIGANESH <113292508+NEDUNURIGANESH@users.noreply.github.com>
2023-09-03 19:19:18 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favoutite foods</h1>
+    <ul>
+      <li>apple</li>
+      <li>banana</li>
+      <li>orange</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
