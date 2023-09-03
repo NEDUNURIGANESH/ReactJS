e7c685ed7e89ed6dbf85f3d0f102a8376c1114cb
Ganesh Nedunuri <ganeshnedunuri3339@gmail.com>
2023-09-03 14:01:50 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My favoutite foods</h1>
-    <ul>
-      <li>apple</li>
-      <li>banana</li>
-      <li>orange</li>
-    </ul>
+    <div>
+      <h1>My favourite foods</h1>
+      <ul>
+        <li>Apple</li>
+        <li>Banana</li>
+        <li>Orange</li>
+      </ul>
+    </div>
+    <div>
+      <h1>creating my name with copyright symbol</h1>
+    <p>Ganesh</p>
+    <p>&copy;2023</p>
+    </div>
   </div>,
   document.getElementById("root")
 );
